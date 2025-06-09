976f8a056ea9b6723e7be373c9e346ab8d4a5c4b
Alfredo Garcia <oxarbitrage@gmail.com>
2025-06-09 16:08:20 -0300

    tests(wallet): Add initial framework to test zallet RPC methods (#9554)
    * build zallet in zebra under a feature
    
    * add dummy wallet test
    
    * use jsonrpc 2.0 in the proxy
    
    * use zallet in wallet test
    
    * minor doc fixes
    
    * fix build
    
    * fix profile to debug in build
    
    * match configs network upgrades
    
    * add issue numbers
    
    * use env var to build zallet instead of a feature
    
    * improve the build a bit
    
    * clippy fix
    
    * update QA docs for zallet
    
    * improve wallet port selection

--- qa/rpc-tests/wallet.py
+++ qa/rpc-tests/wallet.py
@@ -7,7 +7,7 @@
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.authproxy import JSONRPCException
 from test_framework.mininode import COIN
-from test_framework.util import assert_equal, start_nodes, start_node, \
+from test_framework.util import assert_equal, start_nodes, start_wallets, start_node, \
     connect_nodes_bi, sync_blocks, sync_mempools
 from test_framework.zip317 import conventional_fee
 
@@ -21,15 +21,29 @@ class WalletTest (BitcoinTestFramework):
         self.num_nodes = 4
 
     def setup_network(self, split=False):
-        self.nodes = start_nodes(3, self.options.tmpdir, extra_args=[[
-            '-allowdeprecated=getnewaddress',
-            '-allowdeprecated=z_getbalance',
-        ]] * 3)
-        connect_nodes_bi(self.nodes,0,1)
-        connect_nodes_bi(self.nodes,1,2)
-        connect_nodes_bi(self.nodes,0,2)
+        self.nodes = start_nodes(3, self.options.tmpdir)
+
+        # TODO: Connect nodes between them, we need addnode RPC method:
+        # https://github.com/ZcashFoundation/zebra/issues/9555
+        #connect_nodes_bi(self.nodes,0,1)
+        #connect_nodes_bi(self.nodes,1,2)
+        #connect_nodes_bi(self.nodes,0,2)
         self.is_network_split=False
         self.sync_all()
+
+        # If nodes were connected, only one of them would generate a block
+        self.nodes[0].generate(1)
+        self.sync_all()
+
+        # But as we can't connect nodes yet, we need to generate a block to each node manually
+        # TODO: Remove this when we have addnode RPC method:
+        # https://github.com/ZcashFoundation/zebra/issues/9555
+        for i in range(1, len(self.nodes)):
+            self.nodes[i].generate(1)
+
+        # TODO: Wallets can be started but we need to add miner address at least one of them:
+        # https://github.com/ZcashFoundation/zebra/issues/9557
+        self.wallets = start_wallets(3, self.options.tmpdir)
 
     def run_test(self):
         print("Mining blocks...")
@@ -37,10 +51,15 @@ class WalletTest (BitcoinTestFramework):
         self.nodes[0].generate(4)
         self.sync_all()
 
-        walletinfo = self.nodes[0].getwalletinfo()
-        assert_equal(Decimal(walletinfo['immature_balance']), Decimal('40'))
+        walletinfo = self.wallets[0].getwalletinfo()
+        # TODO: getwalletinfo data is not implemented:
+        # https://github.com/zcash/wallet/issues/55
+        # TODO: Miner address is not in the wallet:
+        # https://github.com/ZcashFoundation/zebra/issues/9557
+        #assert_equal(Decimal(walletinfo['immature_balance']), Decimal('40'))
         assert_equal(Decimal(walletinfo['balance']), Decimal('0'))
 
+        """
         blockchaininfo = self.nodes[0].getblockchaininfo()
         assert_equal(blockchaininfo['estimatedheight'], 4)
 
@@ -226,6 +245,7 @@ class WalletTest (BitcoinTestFramework):
         assert_equal(0, len(myvjoinsplits))
         assert("joinSplitPubKey" not in mytxdetails)
         assert("joinSplitSig" not in mytxdetails)
+        """
 
 if __name__ == '__main__':
     WalletTest ().main ()
