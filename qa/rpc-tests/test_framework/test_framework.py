zcash/zcash

commit 2c487c9430450a0ca34fcf5f58c1c938699ff196

Add RPC test for wallet_listunspent changes

qa/rpc-tests/test_framework/test_framework.py
@@ -56,17 +56,18 @@ class BitcoinTestFramework(object):
         # Connect the nodes as a "chain".  This allows us
         # to split the network between nodes 1 and 2 to get
         # two halves that can work on competing chains.
+        connect_nodes_bi(self.nodes, 0, 1)
 
         # If we joined network halves, connect the nodes from the joint
         # on outward.  This ensures that chains are properly reorganised.
-        if not split:
-            connect_nodes_bi(self.nodes, 1, 2)
-            sync_blocks(self.nodes[1:3])
-            if do_mempool_sync:
-                sync_mempools(self.nodes[1:3])
+        if len(self.nodes) >= 4:
+            connect_nodes_bi(self.nodes, 2, 3)
+            if not split:
+                connect_nodes_bi(self.nodes, 1, 2)
+                sync_blocks(self.nodes[1:3])
+                if do_mempool_sync:
+                    sync_mempools(self.nodes[1:3])
 
-        connect_nodes_bi(self.nodes, 0, 1)
-        connect_nodes_bi(self.nodes, 2, 3)
         self.is_network_split = split
         self.sync_all(do_mempool_sync)
 

qa/rpc-tests/test_framework/util.py
@@ -307,15 +307,15 @@ def initialize_chain(test_dir, num_nodes, cachedir, cache_behavior='current'):
         wait_bitcoinds()
         for i in range(MAX_NODES):
             # record the system time at which the cache was regenerated
-            with open(log_filename(cachedir, i, 'cache_config.json'), "w", encoding="utf8") as cache_conf_file:
+            with open(node_file(cachedir, i, 'cache_config.json'), "w", encoding="utf8") as cache_conf_file:
                 cache_config = { "cache_time": time.time() }
                 cache_conf_json = json.dumps(cache_config, indent=4)
                 cache_conf_file.write(cache_conf_json)
 
-            os.remove(log_filename(cachedir, i, "debug.log"))
-            os.remove(log_filename(cachedir, i, "db.log"))
-            os.remove(log_filename(cachedir, i, "peers.dat"))
-            os.remove(log_filename(cachedir, i, "fee_estimates.dat"))
+            os.remove(node_file(cachedir, i, "debug.log"))
+            os.remove(node_file(cachedir, i, "db.log"))
+            os.remove(node_file(cachedir, i, "peers.dat"))
+            os.remove(node_file(cachedir, i, "fee_estimates.dat"))
 
     def init_from_cache():
         for i in range(num_nodes):
@@ -351,7 +351,7 @@ def initialize_chain(test_dir, num_nodes, cachedir, cache_behavior='current'):
         for i in range(MAX_NODES):
             node_path = os.path.join(cachedir, 'node'+str(i))
             if os.path.isdir(node_path):
-                if not os.path.isfile(log_filename(cachedir, i, 'cache_config.json')):
+                if not os.path.isfile(node_file(cachedir, i, 'cache_config.json')):
                     return True
             else:
                 return True
@@ -461,8 +461,8 @@ def start_nodes(num_nodes, dirname, extra_args=None, rpchost=None, binary=None):
         raise
     return rpcs
 
-def log_filename(dirname, n_node, logname):
-    return os.path.join(dirname, "node"+str(n_node), "regtest", logname)
+def node_file(dirname, n_node, filename):
+    return os.path.join(dirname, "node"+str(n_node), "regtest", filename)
 
 def check_node(i):
     bitcoind_processes[i].poll()

qa/pull-tester/rpc-tests.py
@@ -62,6 +62,7 @@ BASE_SCRIPTS= [
     'wallet_overwintertx.py',
     'wallet_persistence.py',
     'wallet_listnotes.py',
+    'wallet_listunspent.py',
     # vv Tests less than 60s vv
     'orchard_reorg.py',
     'fundrawtransaction.py',

qa/rpc-tests/wallet_listunspent.py
@@ -1,0 +1,120 @@
+#!/usr/bin/env python3
+# Copyright (c) 2016-2022 The Zcash developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or https://www.opensource.org/licenses/mit-license.php .
+
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import (
+    assert_equal,
+    get_coinbase_address,
+    nuparams,
+    start_nodes,
+    wait_and_assert_operationid_status,
+    NU5_BRANCH_ID
+)
+
+from decimal import Decimal
+
+class WalletListUnspent(BitcoinTestFramework):
+    def setup_nodes(self):
+        return start_nodes(4, self.options.tmpdir, [[
+            nuparams(NU5_BRANCH_ID, 201),
+        ]] * 4)
+
+    def run_test(self):
+        assert_equal(self.nodes[0].getbalance(), 250)
+        assert_equal(self.nodes[1].getbalance(), 250)
+
+        # Activate NU5
+        self.nodes[1].generate(1) # height 201
+        self.sync_all()
+        assert_equal(self.nodes[0].getbalance(), 260) # additional 10 ZEC matured
+
+        # Shield some coinbase funds so that they become spendable
+        n1acct = self.nodes[1].z_getnewaccount()['account']
+        n1uaddr = self.nodes[1].z_getaddressforaccount(n1acct)['address']
+        opid = self.nodes[0].z_sendmany(
+                get_coinbase_address(self.nodes[0]),
+                [{'address': n1uaddr, 'amount': 10}],
+                1, 0, 'AllowRevealedSenders')
+        wait_and_assert_operationid_status(self.nodes[0], opid)
+
+        self.sync_all()
+        self.nodes[1].generate(2)
+        self.sync_all() # height 203
+        assert_equal(self.nodes[0].getbalance(), 270) # 260 - 10 (spent) + 20 (matured)
+
+        assert_equal(
+                self.nodes[1].z_getbalanceforaccount(n1acct, 1)['pools']['orchard']['valueZat'],
+                Decimal('1000000000'))
+
+        # Get a bare legacy transparent address for node 0
+        n0addr = self.nodes[0].getnewaddress()
+
+        # Send funds to the node 0 address so we have transparent funds to spend.
+        opid = self.nodes[1].z_sendmany(
+                n1uaddr,
+                [{'address': n0addr, 'amount': 10}],
+                1, 0, 'AllowRevealedRecipients')
+        wait_and_assert_operationid_status(self.nodes[1], opid)
+
+        self.sync_all()
+        self.nodes[1].generate(2)
+        self.sync_all() # height 205
+        assert_equal(self.nodes[0].getbalance(), 300) # 270 + 20 (matured) + 10 (received)
+
+        print("----------------------------------------------------------------")
+        unspent_205 = self.nodes[0].listunspent(0, 999999, [])
+        for item in sorted(unspent_205, key=lambda item: item['confirmations']):
+            print(str(item))
+
+        # We will then perform several spends, and then check the list of
+        # unspent notes as of various heights.
+
+        opid = self.nodes[0].z_sendmany(
+                'ANY_TADDR',
+                [{'address': n1uaddr, 'amount': 2}],
+                1, 0, 'AllowRevealedSenders')
+        wait_and_assert_operationid_status(self.nodes[0], opid)
+
+        self.nodes[0].generate(2)
+        self.sync_all() # height 207
+        assert_equal(self.nodes[0].getbalance(), 318) # 300 + 20 (matured) - 2 (sent)
+
+        opid = self.nodes[0].z_sendmany(
+                'ANY_TADDR',
+                [{'address': n1uaddr, 'amount': 3}],
+                1, 0, 'AllowRevealedSenders')
+        wait_and_assert_operationid_status(self.nodes[0], opid)
+
+        self.nodes[0].generate(2)
+        self.sync_all() # height 209
+        assert_equal(self.nodes[0].getbalance(), 335) # 318 + 20 (matured) - 3 (sent)
+
+        opid = self.nodes[0].z_sendmany(
+                'ANY_TADDR',
+                [{'address': n1uaddr, 'amount': 5}],
+                1, 0, 'AllowRevealedSenders')
+        wait_and_assert_operationid_status(self.nodes[0], opid)
+
+        self.nodes[0].generate(2)
+        self.sync_all() # height 211
+        assert_equal(self.nodes[0].getbalance(), 350) # 325 + 20 (matured) - 5 (sent)
+
+        def unspent_total(unspent):
+            total = 0
+            for item in unspent:
+                total += item['amount']
+            return total
+
+        unspent_205 = self.nodes[0].listunspent(0, 999999, [], 205)
+        print("----------------------------------------------------------------")
+        for item in sorted(unspent_205, key=lambda item: item['confirmations']):
+            print(str(item))
+        assert_equal(unspent_total(self.nodes[0].listunspent(0, 999999, [], 205)), 300)
+        assert_equal(unspent_total(self.nodes[0].listunspent(0, 999999, [], 207)), 318)
+        assert_equal(unspent_total(self.nodes[0].listunspent(0, 999999, [], 209)), 335)
+        assert_equal(unspent_total(self.nodes[0].listunspent(0, 999999, [], 211)), 350)
+
+if __name__ == '__main__':
+    WalletListUnspent().main()

src/wallet/rpcwallet.cpp
@@ -2396,7 +2396,7 @@ UniValue listunspent(const UniValue& params, bool fHelp)
 
     if (fHelp || params.size() > 4)
         throw runtime_error(
-            "listunspent ( minconf maxconf  [\"address\",...] unspent_as_of)\n"
+            "listunspent ( minconf maxconf  [\"address\",...] as_of_height)\n"
             "\nReturns array of unspent transparent transaction outputs with between minconf and\n"
             "maxconf (inclusive) confirmations. Use `z_listunspent` instead to see information\n"
             "related to unspent shielded notes. Results may be optionally filtered to only include\n"
@@ -2462,16 +2462,9 @@ UniValue listunspent(const UniValue& params, bool fHelp)
 
     LOCK2(cs_main, pwalletMain->cs_wallet);
 
-    std::optional<int> unspentAsOfDepth;
+    std::optional<int> asOfHeight;
     if (params.size() > 3) {
-        auto asOfHeight = params[3].get_int();
-        // it's fine to specify a height that's greater than the current height
-        if (chainActive.Height() > asOfHeight) {
-            unspentAsOfDepth = chainActive.Height() - asOfHeight;
-            if (unspentAsOfDepth.value() > nMinDepth) {
-                nMinDepth = unspentAsOfDepth.value();
-            }
-        }
+        asOfHeight = params[3].get_int();
     }
 
     UniValue results(UniValue::VARR);
@@ -2485,7 +2478,7 @@ UniValue listunspent(const UniValue& params, bool fHelp)
             false,        // fOnlySpendable
             nMinDepth,
             destinations.empty() ? nullptr : &destinations,
-            unspentAsOfDepth);
+            asOfHeight);
     for (const COutput& out : vecOutputs) {
         if (out.nDepth < nMinDepth || out.nDepth > nMaxDepth)
             continue;

zcash/integration-tests

commit fd769c32b9de9a5c655fbb951fccd14dbf247c18

Regularize process handling logic
This also fixes a bug introduced in 2c487c9430450a0ca34fcf5f58c1c938699ff196
which stopped joining network halves from the joint on outwards.

qa/rpc-tests/addnode.py
@@ -12,21 +12,15 @@ class AddNodeTest (BitcoinTestFramework):
         super().__init__()
         self.cache_behavior = 'clean'
         self.num_nodes = 3
+        self.num_wallets = 0
 
-    def setup_network(self, split=False):
-        args = [None, None, None]
-        self.nodes = start_nodes(self.num_nodes , self.options.tmpdir, extra_args=args)
-
+    def run_test(self):
         # connect all the nodes to each other
-        connect_nodes_bi(self.nodes,0,1)
-        connect_nodes_bi(self.nodes,1,2)
         connect_nodes_bi(self.nodes,0,2)
-        self.is_network_split=split
 
         self.nodes[0].generate(1)
         self.sync_all(False)
 
-    def run_test(self):
         print("Mining blocks...")
 
         # Mine a block from node0

qa/rpc-tests/feature_backup_non_finalized_state.py
@@ -13,11 +13,8 @@ class BackupNonFinalized(BitcoinTestFramework):
     def __init__(self):
         super().__init__()
         self.num_nodes = 1
+        self.num_wallets = 0
         self.cache_behavior = 'clean'
-
-    def setup_network(self):
-        # Start a node with default configuration
-        self.nodes = start_nodes(self.num_nodes, self.options.tmpdir, [None])
 
     def run_test(self):
         self.nodes[0].generate(10)

qa/rpc-tests/feature_nu6.py
@@ -21,13 +21,14 @@ class PoolsTest(BitcoinTestFramework):
     def __init__(self):
         super().__init__()
         self.num_nodes = 1
+        self.num_wallets = 0
         self.cache_behavior = 'clean'
 
-    def setup_network(self):
+    def setup_nodes(self):
         # Add test pre and post NU6 funding streams to the node.
         args = ZebraExtraArgs(funding_streams=[pre_nu6_funding_streams(), post_nu6_funding_streams()]),
 
-        self.nodes = start_nodes(self.num_nodes, self.options.tmpdir, extra_args=args)
+        return start_nodes(self.num_nodes, self.options.tmpdir, extra_args=args)
 
     def run_test(self):
 

qa/rpc-tests/feature_nu6_1.py
@@ -18,6 +18,7 @@ class OnetimeLockboxDisbursementTest(BitcoinTestFramework):
     def __init__(self):
         super().__init__()
         self.num_nodes = 1
+        self.num_wallets = 0
         self.cache_behavior = 'clean'
 
     def start_node_with(self, index, extra_args=[]):
@@ -30,9 +31,8 @@ class OnetimeLockboxDisbursementTest(BitcoinTestFramework):
 
         return start_node(index, self.options.tmpdir, args)
 
-    def setup_network(self, split=False):
-        self.nodes = []
-        self.nodes.append(self.start_node_with(0))
+    def setup_nodes(self):
+        return [self.start_node_with(0)]
 
     def run_test(self):
 

qa/rpc-tests/fix_block_commitments.py
@@ -27,25 +27,28 @@ class BlockCommitmentsTest(BitcoinTestFramework):
     def __init__(self):
         super().__init__()
         self.num_nodes = 2
+        self.num_wallets = 0
         self.cache_behavior = 'clean'
 
     def start_node_with(self, index, args):
         return start_node(index, self.options.tmpdir, args)
 
-    def setup_network(self, split=False):
-        self.nodes = []
+    def setup_nodes(self):
+        nodes = []
 
         # At node 0, Heartwood and NU5 activate in the same block.
         args = ZebraExtraArgs(
             activation_heights={"Heartwood": 1, "NU5": 1}
         )
-        self.nodes.append(self.start_node_with(0, args))
+        nodes.append(self.start_node_with(0, args))
 
         # At node 1, Heartwood and NU5 activate in different blocks.
         args = ZebraExtraArgs(
             activation_heights={"Heartwood": 1, "NU5": 2}
         )
-        self.nodes.append(self.start_node_with(1, args))
+        nodes.append(self.start_node_with(1, args))
+
+        return nodes
 
     def run_test(self):
         print("Activating Heartwood and NU5 in one block at node 0")

qa/rpc-tests/getmininginfo.py
@@ -15,13 +15,8 @@ class GetMiningInfoTest(BitcoinTestFramework):
     def __init__(self):
         super().__init__()
         self.num_nodes = 1
+        self.num_wallets = 0
         self.cache_behavior = 'clean'
-
-    def setup_network(self, split=False):
-        args = [None]
-        self.nodes = start_nodes(self.num_nodes, self.options.tmpdir, args)
-        self.is_network_split = False
-        self.sync_all()
 
     def run_test(self):
         node = self.nodes[0]

qa/rpc-tests/getrawtransaction_sidechain.py
@@ -16,13 +16,8 @@ class GetRawTransactionSideChainTest(BitcoinTestFramework):
     def __init__(self):
         super().__init__()
         self.num_nodes = 1
+        self.num_wallets = 0
         self.cache_behavior = 'clean'
-
-    def setup_nodes(self):
-        return start_nodes(self.num_nodes, self.options.tmpdir)
-
-    def setup_network(self, split=False, do_mempool_sync=True):
-        self.nodes = self.setup_nodes()
 
     def run_test(self):
         n = self.nodes[0]

qa/rpc-tests/indexer.py
@@ -13,16 +13,8 @@ class IndexerTest (BitcoinTestFramework):
         super().__init__()
         self.cache_behavior = 'clean'
         self.num_nodes = 1
-
-    def setup_network(self, split=False):
-        args = [None]
-        self.nodes = start_nodes(self.num_nodes, self.options.tmpdir, args)
-
-        # Zaino need at least 100 blocks to start
-        self.nodes[0].generate(100)
-
-        args = [None]
-        self.zainos = start_zainos(1, self.options.tmpdir)
+        self.num_indexers = 1
+        self.num_wallets = 0
 
     def run_test(self):
         assert_equal(self.zainos[0].getblockcount(), 100)

qa/rpc-tests/nuparams.py
@@ -29,14 +29,8 @@ class NuparamsTest(BitcoinTestFramework):
     def __init__(self):
         super().__init__()
         self.num_nodes = 1
+        self.num_wallets = 0
         self.cache_behavior = 'clean'
-
-    def setup_network(self, split=False):
-        args = [None]
-
-        self.nodes = start_nodes(self.num_nodes, self.options.tmpdir, args)
-        self.is_network_split = False
-        self.sync_all()
 
     def run_test(self):
         node = self.nodes[0]

qa/rpc-tests/test_framework/test_framework.py
@@ -12,20 +12,25 @@ import os
 import sys
 import shutil
 import tempfile
+import time
 import traceback
 
 from .proxy import JSONRPCException
 from .util import (
-    stop_wallets,
-    stop_zainos,
     zcashd_binary,
     initialize_chain,
     start_nodes,
+    start_wallets,
+    start_zainos,
     connect_nodes_bi,
     sync_blocks,
     sync_mempools,
     stop_nodes,
+    stop_wallets,
+    stop_zainos,
     wait_bitcoinds,
+    wait_zainods,
+    wait_zallets,
     enable_coverage,
     check_json_precision,
     PortSeed,
@@ -36,8 +41,11 @@ class BitcoinTestFramework(object):
 
     def __init__(self):
         self.num_nodes = 4
+        self.num_indexers = 0
+        self.num_wallets = 4
         self.cache_behavior = 'current'
         self.nodes = None
+        self.zainos = None
         self.wallets = None
 
     def run_test(self):
@@ -53,32 +61,58 @@ class BitcoinTestFramework(object):
     def setup_nodes(self):
         return start_nodes(self.num_nodes, self.options.tmpdir)
 
+    def prepare_chain(self):
+        if self.num_indexers > 0:
+            # Zaino need at least 100 blocks to start
+            if self.nodes[0].getblockcount() < 100:
+                self.nodes[0].generate(100)
+        elif self.num_wallets > 0:
+            # Zallet needs a block to start
+            if self.nodes[0].getblockcount() < 1:
+                self.nodes[0].generate(1)
+
+    def setup_indexers(self):
+        return start_zainos(self.num_indexers, self.options.tmpdir)
+
+    def setup_wallets(self):
+        return start_wallets(self.num_wallets, self.options.tmpdir)
+
     def setup_network(self, split = False, do_mempool_sync = True):
         self.nodes = self.setup_nodes()
 
         # Connect the nodes as a "chain".  This allows us
         # to split the network between nodes 1 and 2 to get
         # two halves that can work on competing chains.
-        connect_nodes_bi(self.nodes, 0, 1)
 
         # If we joined network halves, connect the nodes from the joint
         # on outward.  This ensures that chains are properly reorganised.
+        if not split and len(self.nodes) >= 3:
+            connect_nodes_bi(self.nodes, 1, 2)
+            sync_blocks(self.nodes[1:3])
+            if do_mempool_sync:
+                sync_mempools(self.nodes[1:3])
+
+        if len(self.nodes) >= 2:
+            connect_nodes_bi(self.nodes, 0, 1)
         if len(self.nodes) >= 4:
             connect_nodes_bi(self.nodes, 2, 3)
-            if not split:
-                connect_nodes_bi(self.nodes, 1, 2)
-                sync_blocks(self.nodes[1:3])
-                if do_mempool_sync:
-                    sync_mempools(self.nodes[1:3])
 
         self.is_network_split = split
+        self.prepare_chain()
         self.sync_all(do_mempool_sync)
+
+        self.zainos = self.setup_indexers()
+        self.wallets = self.setup_wallets()
 
     def split_network(self):
         """
         Split the network of four nodes into nodes 0/1 and 2/3.
         """
         assert not self.is_network_split
+        stop_wallets(self.wallets)
+        wait_zallets()
+        stop_zainos(self.zainos)
+        wait_zainods()
         stop_nodes(self.nodes)
         wait_bitcoinds()
         self.setup_network(True)
@@ -95,11 +129,21 @@ class BitcoinTestFramework(object):
             if do_mempool_sync:
                 sync_mempools(self.nodes)
 
+        # TODO: Sync wallets inside `sync_blocks`
+        # TODO: Use `getwalletstatus` in all sync issues
+        # https://github.com/zcash/wallet/issues/316
+        if self.num_wallets > 0:
+            time.sleep(2)
+
     def join_network(self):
         """
         Join the (previously split) network halves together.
         """
         assert self.is_network_split
+        stop_wallets(self.wallets)
+        wait_zallets()
+        stop_zainos(self.zainos)
+        wait_zainods()
         stop_nodes(self.nodes)
         wait_bitcoinds()
         self.setup_network(False, False)
@@ -162,26 +206,20 @@ class BitcoinTestFramework(object):
         except KeyboardInterrupt as e:
             print("Exiting after " + repr(e))
 
-        print("Stopping wallets")
-        try:
-            if self.wallets:
-                stop_wallets(self.wallets)
-        except Exception as e:
-            print("Ignoring error while stopping wallets: ", repr(e))
-
-        print("Stopping indexers")
-        try:
-            if self.zainos:
-                stop_zainos(self.zainos)
-        except Exception as e:
-            print("Ignoring error while stopping zainos: ", repr(e))
-
         if not self.options.noshutdown:
+            print("Stopping wallets")
+            stop_wallets(self.wallets)
+            wait_zallets()
+
+            print("Stopping indexers")
+            stop_zainos(self.zainos)
+            wait_zainods()
+
             print("Stopping nodes")
             stop_nodes(self.nodes)
             wait_bitcoinds()
         else:
-            print("Note: bitcoinds were not stopped and may still be running")
+            print("Note: zebrads, zainods, and zallets were not stopped and may still be running")
 
         if not self.options.nocleanup and not self.options.noshutdown:
             print("Cleaning up")

qa/rpc-tests/test_framework/util.py
@@ -919,7 +919,17 @@ def stop_wallets(wallets):
             wallet.stop()
         except http.client.CannotSendRequest as e:
             print("WARN: Unable to stop wallet: " + repr(e))
+        except BrokenPipeError as e:
+            print("WARN: Wallet already stopped: " + repr(e))
+        except ConnectionRefusedError as e:
+            print("WARN: Wallet already stopped: " + repr(e))
     del wallets[:] # Emptying array closes connections as a side effect
+
+def wait_zallets():
+    # Wait for all zallets to cleanly exit
+    for zallet in list(zallet_processes.values()):
+        zallet.wait()
+    zallet_processes.clear()
 
 def zallet_config(datadir):
     base_location = os.path.join('qa', 'zallet-datadir')
@@ -963,7 +973,7 @@ def rpc_url_wallet(i, rpchost=None):
 
 # Zaino utilities
 
-zaino_processes = {}
+zainod_processes = {}
 
 def start_zainos(num_nodes, dirname, extra_args=None, rpchost=None, binary=None):
     """
@@ -995,13 +1005,13 @@ def start_zaino(i, dirname, extra_args=None, rpchost=None, timewait=None, binary
 
     args = [ binary, "-c="+config ]
 
-    zaino_processes[i] = subprocess.Popen(args, stderr=stderr)
+    zainod_processes[i] = subprocess.Popen(args, stderr=stderr)
     if os.getenv("PYTHON_DEBUG", ""):
         print("start_node: zainod started, waiting for RPC to come up")
     url = rpc_zaino_url(i, rpchost)
-    wait_for_zainod_start(zaino_processes[i], url, i)
+    wait_for_zainod_start(zainod_processes[i], url, i)
     if os.getenv("PYTHON_DEBUG", ""):
-        print("start_node: RPC successfully started for node {} with pid {}".format(i, zaino_processes[i].pid))
+        print("start_node: RPC successfully started for node {} with pid {}".format(i, zainod_processes[i].pid))
     proxy = get_rpc_proxy(url, i, timeout=timewait)
 
     if COVERAGE_DIR:
@@ -1050,18 +1060,20 @@ def update_zainod_conf(datadir, rpc_port, p2p_port, indexer_port, zaino_rpc_port
     return config_path
 
 def stop_zainos(zainos):
-    # Wait for all zainod processes to exit and clear the process table
-    for proc in list(zaino_processes.values()):
+    # TODO: Add a `stop` RPC method to zainod
+    del zainos[:] # Emptying array closes connections as a side effect
+
+def wait_zainods():
+    # Wait for all zainods to cleanly exit
+    for zainod in list(zainod_processes.values()):
         # TODO: Add a `stop` RPC method to zainod
         try:
-            proc.terminate()
-            proc.wait()
+            zainod.terminate()
+            zainod.wait()
         except Exception:
             try:
-                proc.kill()
+                zainod.kill()
             except Exception:
                 pass
         continue
-    zaino_processes.clear()
-
-    del zainos[:] # Emptying array closes connections as a side effect
+    zainod_processes.clear()

qa/rpc-tests/wallet.py
@@ -18,19 +18,7 @@ class WalletTest (BitcoinTestFramework):
         super().__init__()
         self.cache_behavior = 'clean'
         self.num_nodes = 1
-
-    def setup_network(self, split=False):
-        args = [None]
-        self.nodes = start_nodes(self.num_nodes, self.options.tmpdir, args)
-
-        # Zallet needs a block to start
-        self.nodes[0].generate(1)
-
-        self.wallets = start_wallets(self.num_nodes, self.options.tmpdir)
-
-        # TODO: Use `getwalletstatus` in all sync issues
-        # https://github.com/zcash/wallet/issues/316
-        time.sleep(2)
+        self.num_wallets = 1
 
     def run_test(self):
         # Generate a new account